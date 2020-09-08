78b8eaa0891331e9795c1a1546d49c10730e4829
Mackenzie Smith <smack11231@gmail.com>
2020-09-08 11:22:33 -0600

    finish excercise 1 chapter 10

---
+++ Chapter10/AverageRainFall.java
@@ -0,0 +1,14 @@
+public class Example
+{  
+  public static void main ( String[] args )  
+  {
+    double rain1 = 12;
+    double rain2 = 14;
+    double rain3 = 8;
+    double average = (rain1 + rain2 + rain3)/3;
+    System.out.println("Rainfall for April: \t" + rain1);
+    System.out.println("Rainfall for April: \t" + rain2);
+    System.out.println("Rainfall for April: \t" + rain3);
+    System.out.println("Average Rainfall: \t" + average);
+  }
+}
