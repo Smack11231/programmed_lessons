8b79296b01141e570e9120132cdacddfdec0f698
Mackenzie Smith <smack11231@gmail.com>
2020-09-14 13:55:58 -0600

    complete Exercise 1 Chapter 11

---
+++ Chapter11/Exercise1Ch11.java
@@ -0,0 +1,11 @@
+public class Exercise1Ch11
+{
+  public static void main ( String[] args )
+  {
+    String str;
+    str = new String( "Hello World" );
+    System.out.println( str );
+    String secondvariable;
+    System.out.println( secondvariable );
+  }
+}
