a8b9b5e79614af9a26f226e2a21a9aec8c229d3b
Mackenzie Smith <smack11231@gmail.com>
2020-09-14 14:40:12 -0600

    complete Exercise 4 Chapter 11

---
+++ Chapter11/Exercise4Ch11.java
@@ -0,0 +1,7 @@
+public class Exercise4Ch11 {
+    public static void main ( String[] args) {
+        String str = new String( "Golf is a good walk spoiled." );
+        String sub = str.substring(3, 0);
+        System.out.println( sub );
+    }
+}
