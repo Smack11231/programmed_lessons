8310d9ec14a533f355d400ac5ed14aacd5992d87
Mackenzie Smith <smack11231@gmail.com>
2020-09-14 14:30:18 -0600

    complete Exercise 3 Chapter 11

---
+++ Chapter11/Exercise3Ch11.java
@@ -0,0 +1,9 @@
+public class Exercise3Ch11
+{
+  public static void main ( String[] args )
+  {
+        String first = new String("    In a Hole    in the ground there lived a Hobbit.   ");
+        String second = first.trim();
+        System.out.println("String 1: " + first + "\nString 2: " + second);
+  }
+}
