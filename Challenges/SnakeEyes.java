dbd4930508362edc0f5b61a35ed8ce2444f01e7e
Mackenzie Smith <smack11231@gmail.com>
2020-10-02 11:14:29 -0600

    fixed more and it STILL doesn't work

--- Challenges/SnakeEyes.java
+++ Challenges/SnakeEyes.java
@@ -4,34 +4,34 @@ import java.util.Random;
 public class SnakeEyes {
     public static void main( String[] args) {
         Scanner scan = new Scanner(System.in);
-        Random dice1 = new Random();
-        Random dice2 = new Random();
+        Random dice = new Random();
         System.out.println("How many times do you want the program to run?");
         int times = scan.nextInt();
         scan.close();
 
-        int max = 5;
-        int diceone = dice1.nextInt(max);
-        int dicetwo = dice2.nextInt(max);
-        int sum = 0, avgsum = 0, avgrolls = 0, roll4 = 0;
+        int max = 6;
+        int diceone = dice.nextInt(max);
+        int dicetwo = dice.nextInt(max);
+        int sum = 0;
+        double avgsum = 0, avgrolls = 0, roll5 = 0;
 
-        int i = times;
-        while (i>0) {
-            while (!(diceone == 0) && !(dicetwo==0)) {
+        int i = 0;
+        while (i<times) {
+            while (diceone!=0 && dicetwo!=0) {
                 sum = diceone + dicetwo + 2; // + 2 is  because the min is 0 and the 0 is worth a 1.
                 avgsum += sum;
-                avgrolls ++;
-                diceone = dice1.nextInt(max);
-                dicetwo = dice2.nextInt(max);
+                avgrolls += 1;
+                diceone = dice.nextInt(max);
+                dicetwo = dice.nextInt(max);
             } 
-            i --;
+            i++;
             if (avgrolls > 4) {
-                roll4 ++;
+                roll5++;
             }
         }
         avgrolls = avgrolls / times;
         avgsum = avgsum / times;
-        roll4 = (roll4/times)*100;
-        System.out.println("You had " + avgrolls + " average rolls and a " + avgsum + " average sum. "  + roll4 + "% were plays that had over four rolls.");
+        roll5 = (roll5/times)*100;
+        System.out.println("You had " + avgrolls + " average rolls and a " + avgsum + " average sum. "  + roll5 + "% were plays that had over four rolls.");
     } 
 }
