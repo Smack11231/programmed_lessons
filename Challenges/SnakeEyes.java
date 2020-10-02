5a5c9cc2decc70a804bfcaddd689b044374786bf
Mackenzie Smith <smack11231@gmail.com>
2020-10-02 11:00:29 -0600

    fixing errors but still doesn't work

--- Challenges/SnakeEyes.java
+++ Challenges/SnakeEyes.java
@@ -9,23 +9,29 @@ public class SnakeEyes {
         System.out.println("How many times do you want the program to run?");
         int times = scan.nextInt();
         scan.close();
+
         int max = 5;
         int diceone = dice1.nextInt(max);
         int dicetwo = dice2.nextInt(max);
         int sum = 0, avgsum = 0, avgrolls = 0, roll4 = 0;
-        while (times>0) {
+
+        int i = times;
+        while (i>0) {
             while (!(diceone == 0) && !(dicetwo==0)) {
                 sum = diceone + dicetwo + 2; // + 2 is  because the min is 0 and the 0 is worth a 1.
                 avgsum += sum;
                 avgrolls ++;
+                diceone = dice1.nextInt(max);
+                dicetwo = dice2.nextInt(max);
             } 
-            times --;
+            i --;
             if (avgrolls > 4) {
                 roll4 ++;
             }
         }
         avgrolls = avgrolls / times;
         avgsum = avgsum / times;
-        System.out.println("You had " + avgrolls + " average rolls and a " + avgsum + " average sum. "  + roll4 + " were plays that had over four rolls.");
+        roll4 = (roll4/times)*100;
+        System.out.println("You had " + avgrolls + " average rolls and a " + avgsum + " average sum. "  + roll4 + "% were plays that had over four rolls.");
     } 
 }
