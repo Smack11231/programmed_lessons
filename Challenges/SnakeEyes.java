9d7f40eb8e22bc75fa059a9380c06bc4ae46fff4
Mackenzie Smith <smack11231@gmail.com>
2020-10-06 00:08:22 -0600

    add lines 27 and 28 to fix the problem

--- Challenges/SnakeEyes.java
+++ Challenges/SnakeEyes.java
@@ -10,28 +10,30 @@ public class SnakeEyes {
         scan.close();
 
         int max = 6;
-        int diceone = dice.nextInt(max);
-        int dicetwo = dice.nextInt(max);
-        int sum = 0;
-        double avgsum = 0, avgrolls = 0, roll5 = 0;
+        int diceone = dice.nextInt(max) + 1;
+        int dicetwo = dice.nextInt(max) + 1;
+        double avgsum = 0, avgrolls = 0, roll4 = 0;
 
         int i = 0;
         while (i<times) {
-            while (diceone!=0 && dicetwo!=0) {
-                sum = diceone + dicetwo + 2; // + 2 is  because the min is 0 and the 0 is worth a 1.
-                avgsum += sum;
+            int rolls = 0;
+            while (diceone!=1 && dicetwo!=1) {
+                avgsum += diceone + dicetwo;
                 avgrolls += 1;
                 diceone = dice.nextInt(max);
                 dicetwo = dice.nextInt(max);
+                rolls++;
             } 
-            i++;
-            if (avgrolls > 4) {
-                roll5++;
+            diceone = dice.nextInt(max);
+            dicetwo = dice.nextInt(max);
+            if (rolls > 4) {
+                roll4++;
             }
+            i++;
         }
-        avgrolls = avgrolls / times;
-        avgsum = avgsum / times;
-        roll5 = (roll5/times)*100;
-        System.out.println("You had " + avgrolls + " average rolls and a " + avgsum + " average sum. "  + roll5 + "% were plays that had over four rolls.");
+        avgrolls /= times;
+        avgsum /= times;
+        roll4 = (roll4/times)*100;
+        System.out.println("You had " + avgrolls + " average rolls and a " + avgsum + " average sum. "  + roll4 + "% were plays that had over four rolls.");
     } 
 }
