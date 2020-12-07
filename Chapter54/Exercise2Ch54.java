961beae35cbcc2e8907070c333e1c6335bf9668e
Mackenzie Smith <smack11231@gmail.com>
2020-12-07 01:23:47 -0700

    finish exercise 2

---
+++ Chapter54/Exercise2Ch54.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise2Ch54{
+
+    public static boolean isSafe(long num) {
+        String remainder = String.valueOf(num);
+        return remainder.contains("13");
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        long num = scan.nextLong();
+
+		if( isSafe( num ) ) {
+			System.out.println( num + " is unlucky" );
+		}
+		else {
+			System.out.println( num + " is safe" );
+        }
+        
+        scan.close();
+    }
+}
