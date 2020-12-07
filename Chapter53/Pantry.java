2c3b27f6cab6436c1eddfd183fe531b87ddb583f
Mackenzie Smith <smack11231@gmail.com>
2020-12-07 00:43:57 -0700

    Finish Exercise2

--- Chapter53/Pantry.java
+++ Chapter53/Pantry.java
@@ -13,29 +13,55 @@ public class Pantry {
     selected = null ;
   }
 
+  public Pantry(Jam jar1, Jam jar2){
+    this.jar1 = jar1;
+    this.jar2 = jar2;
+    this.jar3 = null;
+    selected = null;
+  }
+
+  public Pantry(Jam jar1){
+    this.jar1 = jar1;
+    this.jar2 = null;
+    this.jar3 = null;
+    selected = null;
+  }
+
   // Methods
   public String toString() {
     String str = "";
-    str += "The jams are:\n1: " +  jar1.toString()  + "\n";
-    str += "2: " +  jar2.toString()  + "\n";
-    str += "3: " +  jar3.toString()  + "\nEnter your selection (1, 2, or 3):";     
+    if (jar1 != null) str += "The jams are:\n1: " + jar1.toString() + "\n";
+    if (jar2 != null) str += "2: " + jar2.toString() + "\n";
+    if (jar3 != null) str += "3: " + jar3.toString() + "\nEnter your selection (1, 2, or 3):";        
     return str;
   }
 
   // assume that the user entered a correct selection, 1, 2, or 3
-  public void select( int jarNumber ) {
-    if ( jarNumber == 1 )
-      selected =  jar1 ;
-
-    else if ( jarNumber == 2 )
-      selected = jar2 ;
-
-    else 
-      selected = jar3 ;
-  }
 
   // spread the selected jam
   public void spread( int oz ) {
     selected . spread( oz ) ;
   }
+
+  public boolean select(int jarNumber){
+    if (jarNumber == 1){
+        if (jar1 != null){
+          selected =  jar1;
+          return true;
+        }
+      } else if (jarNumber == 2){
+        if (jar2 != null){
+          selected = jar2;
+          return true;
+        }
+        return false;
+      } else {
+          if (jar3 != null){
+            selected = jar3;
+            return true;
+          }
+          return false;
+      }
+    return false;
+}
 }
