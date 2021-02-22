6f70b4b25c87171ffe8b9b35aac9a1ec66378af3
Mackenzie Smith <smack11231@gmail.com>
2021-02-22 00:48:20 -0700

    finish ex6

--- Chapter91-94/Chapter91/ex6.java
+++ Chapter91-94/Chapter91/ex6.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ex5 {
+public class ex6 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter n:");
